bb0fd262018c68e94010f44e48fd37cfcdad620e
Jax3nn <jcorbyb@gmail.com>
2026-02-01 19:23:04 +0000

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dcyaztqsq/video/upload/v1769973620/ytmp3free.cc_beabadoobee-the-perfect-pair-official-audio-youtubemp3free.org_lc1yio.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
